5f22cac7f8a19c934045c718807cf43bb44271e2
HernanC95 <cavalieri.h95@gmail.com>
2022-10-17 21:18:12 -0300

    .\past-events.js

--- past-events.js
+++ past-events.js
@@ -1,41 +1,49 @@
-let fechaActual = data.currentDate
-let eventos = data.events
+let fechaActual = data.currentDate;
+let eventos = data.events;
 
-function validarFechaPast(fechaEvento,currentDate){
-    let resultado=false
-    if (fechaEvento<currentDate){
-        resultado=true
+function getEvents(){
+  return data.events
+}
+
+
+
+let eventosPasados = getEvents().filter(function (event) {
+    if (event.date <= fechaActual) {
+      return event
     }
-    return resultado
+  });
+
+let container = document.getElementById("container");
+
+eventosPasados.forEach(imprimirCartas)
+
+function imprimirCartas(carta) {
+  let article = document.createElement("article");
+  article.innerHTML += `    <div class="card h-100" style="width: 18rem">
+      <img
+        src="${carta.image}"
+        class="card-img-top p-2 h-50 rounded-4"
+        alt="${carta.name}"
+      />
+      <div
+        class="card-body d-flex flex-column align-items-center justify-content-between"
+      >
+        <h5 class="card-title">${carta.name}</h5>
+        <p class="card-text text-center">${carta.description}</p>
+        <div
+          class="d-flex w-100 justify-content-between align-items-center"
+        >
+          <p class="m-0">Price: ${carta.price}</p>
+          <a href="#" class="btn btn-secondary">Details</a>
+        </div>
+      </div>
+    </div>`;
+  container.appendChild(article);
 }
 
-function filtrarPastEvents(eventos) {
-  let container = document.getElementById('container')
-  for (let i = 0; i < eventos.length; i++) {
-    if (validarFechaPast(eventos[i].date,fechaActual)){
-      let article = document.createElement('article')
-        article.innerHTML +=
-      `    <div class="card h-100" style="width: 18rem">
-            <img
-              src="${eventos[i].image}"
-              class="card-img-top p-2 h-50 rounded-4"
-              alt="${eventos[i].name}"
-            />
-            <div
-              class="card-body d-flex flex-column align-items-center justify-content-between"
-            >
-              <h5 class="card-title">${eventos[i].name}</h5>
-              <p class="card-text text-center">${eventos[i].description}</p>
-              <div
-                class="d-flex w-100 justify-content-between align-items-center"
-              >
-                <p class="m-0">Price: ${eventos[i].price}</p>
-                <a href="#" class="btn btn-secondary">Details</a>
-              </div>
-            </div>
-          </div>`
-        container.appendChild(article)
-      }
-  }
-}
-filtrarPastEvents(eventos)
+const inputSearch = document.getElementById ("js-search")
+inputSearch.addEventListener("input", function (event) {
+ let aux =eventosPasados. filter((evento) => evento.name.toLowerCase().includes(event.target.value.toLowerCase()))
+  container.innerHTML = ``
+  aux.forEach(imprimirCartas)
+}) 
