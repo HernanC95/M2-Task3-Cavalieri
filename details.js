c8e36cb17f3f1eee9d38d6f040d125fc16b1dedf
HernanC95 <cavalieri.h95@gmail.com>
2022-10-19 02:32:55 -0300

    .\details.js

--- details.js
+++ details.js
@@ -8,30 +8,33 @@ let container = document.getElementById("container");
 function imprimirCartas(carta) {
   let article = document.createElement("article");
   article.innerHTML += ` 
-                <article class="d-flex flex-column justify-content-center align-items-center text-white border p-4 colorHeader rounded-5" style="width: 25rem">
-                    <h1 class="p-2">${carta.name}</h1>
-                    <img src="${carta.image}" class="rounded-5 p-3" style="width: 25rem" alt="${carta.name}"></img>
-                    <div class="d-flex flex-wrap justify-content-between w-100">
-                    <p>Fecha: ${carta.date}</p>
-                    <p>Categoria: ${carta.category}</p>
+                <article class="d-flex flex-wrap justify-content-center align-items-center text-white border p-2 colorHeader rounded-5">
+                <img src="${carta.image}" class="rounded-5 img-details" style="width: 40vw" alt="${carta.name}"></img>
+                <div class="d-flex flex-column flex-wrap justify-content-between p-2">    
+                    <h1 class="p-2 text-center fw-bold">${carta.name}</h1>
+                    <div class="d-flex flex-wrap justify-content-around p-5">
+                    <p><span class="fw-bold p-2">Fecha:</span>  ${carta.date}</p>
+                    <p><span class="fw-bold p-2">Categoria:</span>  ${carta.category}</p>
                     </div>
-                    <p class="text-center">
+                    <p class="text-center w-75 align-self-center">
+                    <span class="fw-bold">Description:</span> 
                     ${carta.description}
                     </p>
-                    <div class="d-flex flex-wrap justify-content-between w-75">
-                      <p>Place: ${carta.place}</p>
-                      <p>Price: ${carta.price}</p>
+                    <div class="d-flex flex-wrap justify-content-around p-5">
+                      <p><span class="fw-bold p-2">Place:</span>  ${carta.place}</p>
+                      <p><span class="fw-bold p-2">Price:</span>  ${carta.price}</p>
+                    </div>
                     </div>
                 </article>`;
 
   container.appendChild(article);
 }
 
-async function getEvents1() {
+async function getLocation() {
   let id = Number(location.search.slice(4));
   console.log(id);
   let event = getEvents().filter((evento) => evento._id === id);
   imprimirCartas(event[0]);
 }
 
-getEvents1();
+getLocation();
